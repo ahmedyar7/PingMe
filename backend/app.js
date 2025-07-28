563983b857d5a1e5d3837ed27f666dcd1f86eae6
ahmedyar7 <ahmedyar778@gmail.com>
2025-07-28 20:13:22 +0500

    handling the cors error

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,14 @@ const app = express();
 
 const __dirname = path.resolve();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ping-me-q32q.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
